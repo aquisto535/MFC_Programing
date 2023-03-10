a98e9de7640cd526814fd26a0db98a5938746f29
Jung Tae Min <71391013+aquisto535@users.noreply.github.com>
2023-03-10 17:03:02 +0900

    2023-03-10
    study GDIs

--- GDI_Basic/ChildView.cpp
+++ GDI_Basic/ChildView.cpp
@@ -53,9 +53,64 @@ void CChildView::OnPaint()
 	
 	// 그리기 메시지에 대해서는 CWnd::OnPaint()를 호출하지 마십시오.
 
-	CDC* pDC = GetDC();
-	pDC->Rectangle(10,10,110,110);
-	ReleaseDC(pDC);
+	//CDC* pDC = GetDC(); 동적 생성
+
+	//CPen NewPen(PS_SOLID, 3, RGB(192, 0, 0));
+	//CPen* pOldPen = dc.SelectObject(&NewPen); //dc가 내가 정한 펜을 선택하도록 설정한 펜의 주소를 기존 펜의 값에 대입.
+	//CBrush NewBrush(RGB(192, 192,192));
+	//CBrush *pOldBrush = dc.SelectObject(&NewBrush);
+
+	//dc.Rectangle(10,10,110,110);
+
+	//dc.SelectObject(pOldPen);//지금까지 그린 직사각형 대상을 반환. dc.SelectObject()는 반드시 본래의 객체에 대한 주소를 리턴한다.
+	//dc.SelectObject(pOldBrush);
+
+	//dc.Rectangle(10, 10, 110, 110);
+
+	//ReleaseDC(pDC); 동적 생성 후 해제
+
+/*	LOGBRUSH lb;
+	lb.lbStyle = BS_SOLID;
+	lb.lbColor = RGB(192,0 , 0);
+	DWORD style[] = { 1,1 };*/ // 픽셀의 길이, 빈 공간의 길이(lpStyle). 배열의 선언방식으로 지정.
+/*
+	CPen NewPen;
+	NewPen.CreatePen( PS_USERSTYLE | PS_GEOMETRIC | PS_ENDCAP_FLAT, 1, &lb, 2, style);*/// nStyleCount= 항의 갯수. lpStyle은 이중 단어 값 배열을 가리킵니다. 첫 번째 값은 사용자 정의 스타일에서 첫 번째 대시의 길이를 지정하고, 두 번째 값은 첫 번째 공간의 길이 등을 지정합니다.이 포인터는 NULL 그렇지 않은 PS_USERSTYLE경우 nPenStyle 여야 합니다.
+
+	//CPen NewPen(PS_SOLID | PS_GEOMETRIC | PS_ENDCAP_FLAT , 10(두께),  &lb);// 미리 지정된 속성값을 또 대입하면 오류가 날 수 있다!! ex) RGB(192, 192, 192) 중복 대입.
+	//CPen* pOldPen = dc.SelectObject(&NewPen);  
+	//CBrush NewBrush(HS_CROSS ,RGB(0, 192, 0));
+	//CBrush* pOldBrush = dc.SelectObject(&NewBrush);
+
+	//dc.FillSolidRect(20,20, 100, 100, RGB(0,0,0));//사각형을 단색으로 채웁니다.펜을 사용하지 않고도 도형을 그릴 수 있음.
+	//dc.SetBkMode(TRANSPARENT);
+	//dc.Rectangle(20, 20, 120, 120);
+
+	//for (int i = 0; i < 10; i++) //반복문을 통해 화면 전체를 그릴 수도 있다!!
+	//{
+	//	dc.MoveTo(40, 12* i + 40);
+	//	dc.LineTo(240, 12 * i + 40);
+
+	//}
+
+	CBrush NewBrush;
+	CBitmap Bmp;
+	Bmp.LoadBitmap(IDB_BITMAP1);
+	NewBrush.CreatePatternBrush(&Bmp);
+	CBrush* pOldBrush = dc.SelectObject(&NewBrush);
+
+	CRect Rect; 
+	GetClientRect(&Rect);
+	dc.Rectangle(&Rect);
+
+	//dc.SelectObject(pOldPen);
+	dc.SelectObject(pOldBrush);
+	dc.SetBkMode(TRANSPARENT);
+	dc.SetTextColor(RGB(0,0,255));
+	dc.TextOutW(100, 100, _T("TEST STRING"));
+
+	//dc.MoveTo(40, 40);//본래 속성으로 돌아감
+	//dc.LineTo(240, 40);//본래 속성으로 돌아감
 }
 
 
---
+++ GDI_도형/ChildView.cpp
@@ -0,0 +1,111 @@
+﻿
+// ChildView.cpp: CChildView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "GDI_도형.h"
+#include "ChildView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChildView
+
+CChildView::CChildView()
+{
+}
+
+CChildView::~CChildView()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+END_MESSAGE_MAP()
+
+
+
+// CChildView 메시지 처리기
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs) 
+{
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW|CS_VREDRAW|CS_DBLCLKS, 
+		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW+1), nullptr);
+
+	return TRUE;
+}
+
+void CChildView::OnPaint() 
+{
+	CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+	
+	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
+	
+	// 그리기 메시지에 대해서는 CWnd::OnPaint()를 호출하지 마십시오.
+
+	LOGFONT lf;
+	::ZeroMemory(&lf, sizeof(lf));
+	lf.lfHeight = 30;
+	wsprintf(lf.lfFaceName, _T("%s"), _T("Arial"));
+
+	CFont NewFont;
+	NewFont.CreateFontIndirectW(&lf);
+	CFont* pOldFont = dc.SelectObject(&NewFont);
+
+	//dc.TextOutW(10, 10, _T("Test String!!"));
+	//dc.TextOutW(10, 50, _T("테스트 문자열입니다."));
+	
+	int arnTabStop[2] = {10,40};
+	CSize size = dc.TabbedTextOutW(10,10, _T("\t\t테스트 문자열입니다."), 2, arnTabStop,10);// 탭 구현하는 코드. 다채로운 색깔을 구현하려면 TabbedTextOutW을 써야 한다!!
+
+	dc.SelectObject(pOldFont);//마지막에 출력해주어야 함.
+	//CRect Rect;
+	//GetClientRect(&Rect);
+
+	//dc.FillSolidRect(&Rect, RGB(0, 0, 255));
+
+	//CPen NewPen(PS_SOLID, 1, RGB(0,0,0));
+	//CPen* pOldPen = dc.SelectObject(&NewPen);
+	//
+	//Rect -= CRect(10,10,10,10);
+	//dc.RoundRect(Rect, CPoint(30, 30)); //Rect를 넣어도 됨.
+	//dc.SelectObject(pOldPen);
+	
+	//CBrush* pOldBrush = dc.SelectObject(&NewBrush); 
+
+	//dc.Rectangle(20, 20, 140, 140);
+	//dc.Ellipse(20, 20, 140, 140);
+
+	//dc.BeginPath();// 삼각형 그릴 때 무조건 있어야함! 단순히 선만 긋는다고 삼각형이 되는 것이 아니다!! 두깨가 20픽셀인 선을 세 번 그어 삼각형을 만든다.//사각형도 비슷하다.
+	//dc.MoveTo(20, 20);
+	//dc.LineTo(140, 20);
+	//dc.LineTo(140, 140);
+	//dc.LineTo(20, 140);
+	//dc.LineTo(20, 20);
+	//dc.EndPath();// 삼각형 그릴 때 무조건 있어야함!
+	//dc.StrokeAndFillPath();// 삼각형 그릴 때 무조건 있어야함! 자동으로 렌더링해줌!!
+
+	//삼각형 그리는 다른 방법
+	//POINT arPt[3] = { {190,43}, {290,190}, {90, 190} };
+	//dc.Polygon(arPt, 3);
+
+	//dc.SelectObject(pOldBrush); //브러시
+	//dc.SelectObject(pOldPen); //펜
+
+	//기본 펜으로 삼각형을 겹쳐 그린다.
+	//dc.MoveTo(20, 20);
+	//dc.LineTo(140, 20);
+	//dc.LineTo(140, 140);
+	//dc.LineTo(20, 140);
+	//dc.LineTo(20, 20);
+}
+
